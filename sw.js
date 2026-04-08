1f1665dc1cc3beac527819eb4fc8ff306bb7032a
monolitos-agenda <airtonsantiago1001@gmail.com>
2026-04-07 22:00:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE = 'monolitos-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=DM+Sans:wght@300;400;500;600&display=swap'
-];
+const CACHE = 'monolitos-v2';
+const ASSETS = ['./','./index.html','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll(ASSETS.filter(a => !a.startsWith('http')));
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
-    })
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>caches.match('./index.html')))
   );
 });
